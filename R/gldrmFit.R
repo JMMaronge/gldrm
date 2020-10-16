c5cf1ff89a6a9069a31a8469f7aed8113f19ceda
jmmaronge <jmmaronge@gmail.com>
2020-10-16 13:11:51 -0500

    finally fixed?

--- R/gldrmFit.R
+++ R/gldrmFit.R
@@ -430,12 +430,8 @@ gldrmFit <- function(x, y, linkfun, linkinv, mu.eta, mu0=NULL, offset=NULL, samp
 		if(length.betas==1){
       print("length beta = 1")
 		  infobeta <- as.double(infobeta)
-		  
-		  print(t(U1))
-		  print(infocross)
-		  print(U1)
-		  print(crossprod(U1, infocross))
-		  tmp <- as.double(infobeta*(t(U1)%*%U1)) + t(U2)%*%t(infocross)%*%U1 + crossprod(U1, infocross)%*%U2 + t(U2)%*%infof0%*%U2  
+		  U1 <- t(U1) # this technically not mathematically correct, doing this because R forces U1 to column vector when it should be row
+		  tmp <-  t(U1)%*%U1*%*%infobeta + t(U2)%*%t(infocross)%*%U1 + t(U1)%*%infocross%*%U2 + t(U2)%*%infof0%*%U2}  
 		} else{
 
 		  print(infobeta)
